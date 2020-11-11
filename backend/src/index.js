568d628388350dbdc382135c74c2cf73425e5193
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-11-11 01:27:33 -0300

    adicionando alteracoes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { uuid, isUuid } = require('uuidv4');
 
 
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 /**
  * metodos http:Get
